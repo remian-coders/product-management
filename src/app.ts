f170aef1b7ffb6a747586eba5a3da4fabff7d3c1
Asliddin Buriev <asliddin00buriev@gmail.com>
2022-07-13 02:05:39 +0900

    complete client register

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,8 @@
 import express, { Express } from 'express';
 import csvPathRouter from './routes/csv-path.routes';
+import clientRegisterRouter from './routes/client-register.routes';
+import errorHandler from './controllers/error.controller';
+import CustomErrorGenerator from './utils/custom-error-generator';
 export class App {
 	app: Express;
 	constructor() {
@@ -13,8 +16,12 @@ export class App {
 	}
 	routes() {
 		this.app.use('/api/csv-path', csvPathRouter);
+		this.app.use('/api/client-register', clientRegisterRouter);
+		this.app.use('*', (req, res, next) => {
+			next(new CustomErrorGenerator('there is no such route', 404));
+		});
 	}
 	errorHandler() {
-		console.log('errorHandler');
+		this.app.use(errorHandler);
 	}
 }
---
+++ src/utils/custom-error-generator.ts
@@ -0,0 +1,11 @@
+export default class CustomErrorGenerator extends Error {
+	status: 'fail' | 'error';
+	isOperational: boolean;
+	constructor(public message: string, public statusCode: number) {
+		super(message);
+		this.statusCode = statusCode;
+		this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error';
+		this.isOperational = true;
+		Error.captureStackTrace(this, this.constructor);
+	}
+}
