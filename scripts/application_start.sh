17ecb86eba882cc0a1daaadb014e381795582557
Asliddin Buriev <asliddin00buriev@gmail.com>
2022-07-23 00:10:12 +0900

    change deployment script

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -3,11 +3,8 @@
 echo 'Starting application...'
 cd /home/ec2-user/promos
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
-export NVM_DIR="$HOME/.nvm"	
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  
+. ~/.nvm/nvm.sh
 nvm install --lts
-
 cd /home/ec2-user/promos
 sudo npm install --g lerna
 sudo npm install
