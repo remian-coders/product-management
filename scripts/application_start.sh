5c7378ad605bcd4734fbb9c68397c3c3531a3a1e
Asliddin Buriev <asliddin00buriev@gmail.com>
2022-07-22 21:14:02 +0900

    change scripts

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -4,11 +4,10 @@ sudo chmod -R 777 /home/ec2-user/promos
 
 cd /home/ec2-user/promos
 
-#add npm and node to path
-export NVM_DIR="$HOME/.nvm"	
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm	
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
 
+export NVM_DIR="$HOME/.nvm"	
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  
 
 cd /home/ec2-user/promos
 npm install --g lerna
