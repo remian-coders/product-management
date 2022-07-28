14e8ae6ff42738b5114fce01c54d449c8929fb2b
Abdulvokhid <mr.ochilov1997@gmail.com>
2022-07-28 19:57:54 +0900

    url change

--- packages/front/src/utils/api.js
+++ packages/front/src/utils/api.js
@@ -2,18 +2,18 @@ import axios from "axios";
 
 const getBaseUrl = () => {
   let url;
-  // switch (process.env.NODE_ENV) {
-  //   case "production":
-  //     url = "http://43.200.179.237:5000/api";
-  //     break;
-  //   case "development":
-  //   default:
-  //     url = "http://localhost:5000/api";
-  // }
+  switch (process.env.NODE_ENV) {
+    case "production":
+      url = "http://43.200.179.237:5000/api";
+      break;
+    case "development":
+    default:
+      url = "http://localhost:5000/api";
+  }
 
   return url;
 };
 
 export default axios.create({
-  baseURL: "http://43.200.179.237:5000/api",
+  baseURL: getBaseUrl(),
 });
