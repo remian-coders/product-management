cf9907de6c3574f53e83e4d46c58457518e97354
Asliddin Buriev <asliddin00buriev@gmail.com>
2022-08-03 23:53:06 +0900

    add logs to debug

--- packages/back/src/controllers/csv-file.controller.ts
+++ packages/back/src/controllers/csv-file.controller.ts
@@ -12,6 +12,7 @@ export const addPath = catchAsyncError(
 	async (req: Request, res: Response, next: express.NextFunction) => {
 		const { path: csvFolder } = req.body;
 		console.log('******', csvFolder);
+		console.log('******', req.body);
 		const isExist = await access(csvFolder, constants.R_OK | constants.W_OK)
 			.then(() => true)
 			.catch(() => false);
