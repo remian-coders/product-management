551f1513651b4f115992fd0865225a9333294792
Asliddin Buriev <asliddin00buriev@gmail.com>
2022-08-31 19:44:35 +0900

    update local with remote

--- packages/front/src/utils/api.js
+++ packages/front/src/utils/api.js
@@ -4,13 +4,13 @@ const getBaseUrl = () => {
 	let url;
 	switch (process.env.NODE_ENV) {
 		case 'production':
-			url = 'http://192.168.0.197:5000/api';
-			// url = 'http://localhost:5000/api';
+			// url = 'http://192.168.0.197:5000/api';
+			url = 'http://localhost:5000/api';
 			break;
 		case 'development':
 		default:
-			url = 'http://192.168.0.197:5000/api';
-		// url = 'http://localhost:5000/api';
+			// url = 'http://192.168.0.197:5000/api';
+			url = 'http://localhost:5000/api';
 	}
 
 	return url;
