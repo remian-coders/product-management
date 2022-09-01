7c3b6eac0ab47906adadd85ae060efea7dc6e88c
Asliddin Buriev <asliddin00buriev@gmail.com>
2022-09-01 21:15:36 +0900

    commit for testing

--- packages/front/src/utils/api.js
+++ packages/front/src/utils/api.js
@@ -4,8 +4,8 @@ const getBaseUrl = () => {
 	let url;
 	switch (process.env.NODE_ENV) {
 		case 'production':
-			// url = "http://192.168.0.197:5000/api";
-			url = 'http://localhost:5000/api';
+			url = 'http://192.168.0.197:5000/api';
+			// url = 'http://localhost:5000/api';
 			break;
 		case 'development':
 		default:
